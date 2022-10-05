fdce775a0773fe5f54292850c9710841dffa2276
Buldakov Ilya <jdoe@example.com>
2022-10-05 15:26:19 +0400

    Fixed convert() method return type

--- src/main/java/services/SizeConverter.java
+++ src/main/java/services/SizeConverter.java
@@ -1,18 +1,15 @@
 package main.java.services;
 
 import main.java.models.Measure;
-
-import java.util.Map;
-
 public class SizeConverter {
 
-    public static Map<String, Measure> convert(float input) {
+    public static String convert(float input) {
         // Division by 1024 counter.
         int counter = 0;
         while (input > 1024) {
             input /= 1024;
             counter++;
         }
-        return Map.of(String.format("%.2f", input), Measure.values()[counter]);
+        return String.format("%.2f " + Measure.values()[counter], input);
     }
 }
