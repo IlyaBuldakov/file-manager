eb7d8383cd414e5fd368cf4153fe7444b61fbd74
Buldakov Ilya <jdoe@example.com>
2022-10-05 15:39:21 +0400

    Added interface for models capable display view

---
+++ src/main/java/models/Viewable.java
@@ -0,0 +1,13 @@
+package main.java.models;
+
+/**
+ * Interface implemented by model
+ * classes capable of returning a view.
+ */
+public interface Viewable {
+
+    /**
+     * View-return method.
+     */
+    void displayView();
+}
