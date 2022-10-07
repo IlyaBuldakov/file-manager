8362dddf9b0005a217e9a5180fa8a98136fa0ba2
Buldakov Ilya <jdoe@example.com>
2022-10-07 17:57:47 +0400

    Added the ability to navigate to directories by number

--- src/main/java/controllers/InputController.java
+++ src/main/java/controllers/InputController.java
@@ -13,18 +13,18 @@ import java.util.concurrent.CompletableFuture;
  */
 public class InputController {
 
+    FileTree fileTree = FileTreeBuilder.build(System.getProperty("user.home"));
+
     /**
      * The greeting method that is called when the
      * program starts before interacting with the user.
      */
     private void greetingPage() {
-        System.out.println();
-        System.out.println("***************************************");
-        System.out.println("*** Welcome! Here is your home path ***");
-        System.out.println("***************************************");
-
         CompletableFuture<Void> greetingUserDig = CompletableFuture.supplyAsync(() -> {
-            FileTree fileTree = FileTreeBuilder.build(System.getProperty("user.home"));
+            System.out.println();
+            System.out.println("***************************************");
+            System.out.println("*** Welcome! Here is your home path ***");
+            System.out.println("***************************************");
             fileTree.displayView();
             return null;
         });
@@ -43,8 +43,16 @@ public class InputController {
             if (input.equalsIgnoreCase("exit")) {
                 break;
             }
-            FileTree fileTree = FileTreeBuilder.build(input);
-            fileTree.displayView();
+            try {
+                FileTree localTree = FileTreeBuilder
+                        .build(fileTree
+                                .getTree().get(Integer.parseInt(input) - 1).getPath().toString());
+                localTree.displayView();
+            } catch (NumberFormatException exception) {
+                FileTree localTree = FileTreeBuilder
+                        .build(input);
+                localTree.displayView();
+            }
         }
     }
 }
