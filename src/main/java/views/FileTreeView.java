6f202ba049c93fa319322046d6e1c5b2b3fa4894
Buldakov Ilya <jdoe@example.com>
2022-10-07 17:57:11 +0400

    Added objects numbering and head name in output

--- src/main/java/views/FileTreeView.java
+++ src/main/java/views/FileTreeView.java
@@ -16,17 +16,20 @@ public class FileTreeView {
     public String toString() {
         StringBuilder sb = new StringBuilder();
         List<Information> list = tree.getTree();
-        for (Information info : list) {
-            sb.append(new InformationDto(info));
+        for (int i = 0; i < list.size(); i++) {
+            sb.append(i + 1);
+            sb.append(" | ");
+            sb.append(new InformationDto(list.get(i)));
         }
-        return "=== File tree === \n" +
+        return "=== File tree " + tree.getHeadName() + " === \n" +
                 sb + "\n" +
-                "=== File tree ===";
+                "=== File tree " + tree.getHeadName() + " ===";
     }
 
     /**
      * Constructor that creates view
      * and display in console.
+     *
      * @param tree FileTree.
      */
     public FileTreeView(FileTree tree) {
