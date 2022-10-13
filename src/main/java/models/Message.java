7d79319f94cc676da4292cda9eb15f7a6b717ee4
Buldakov Ilya <jdoe@example.com>
2022-10-13 10:59:43 +0400

    Added new error enum

--- src/main/java/models/Message.java
+++ src/main/java/models/Message.java
@@ -7,7 +7,9 @@ public enum Message {
 
     INTERNAL_ERROR("Internal error."),
 
-    IO_ERROR("Input/output error. Perhaps the file does not exist or the path is incorrect.");
+    IO_ERROR("Input/output error. Perhaps the file does not exist or the path is incorrect."),
+
+    SIZE_HANDLER_ERROR("Size handler error.");
 
     private final String text;
 
