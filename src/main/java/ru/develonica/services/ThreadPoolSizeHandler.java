6c99f16727912d4f7ef69e3d84652520474e022b
Buldakov Ilya <jdoe@example.com>
2022-10-20 11:47:22 +0400

    Fixed size handler algorithm

--- src/main/java/ru/develonica/services/ThreadPoolSizeHandler.java
+++ src/main/java/ru/develonica/services/ThreadPoolSizeHandler.java
@@ -1,15 +1,15 @@
 package ru.develonica.services;
 
-import ru.develonica.models.Message;
-import ru.develonica.views.ErrorView;
-
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.stream.Collectors;
 
 public class ThreadPoolSizeHandler implements SizeHandler {
 
@@ -18,24 +18,25 @@ public class ThreadPoolSizeHandler implements SizeHandler {
     public float activate(File destination) throws ExecutionException, InterruptedException, IOException {
         File[] files = destination.listFiles();
         float filesSize = 0f;
-        if (files != null) {
+        if (files != null && files.length != 0) {
+            List<File> dest = new ArrayList<>();
             for (File file : files) {
                 if (file.isFile()) {
                     filesSize += Files.size(file.toPath());
                 } else {
-                    filesSize += CompletableFuture.supplyAsync(() -> {
-                        try {
-                            return activate(file);
-                        } catch (ExecutionException | InterruptedException e) {
-                            ErrorView.displayError(Message.SIZE_HANDLER_ERROR);
-                            return 0f;
-                        } catch (IOException e) {
-                            ErrorView.displayError(Message.IO_ERROR);
-                            return 0f;
-                        }
-                    }, threadPool).get();
+                    dest.add(file);
                 }
             }
+            List<CompletableFuture<Float>> cfList = dest.stream()
+                    .map(file -> CompletableFuture.supplyAsync(() -> {
+                        try {
+                            return activate(file);
+                        } catch (ExecutionException | InterruptedException | IOException e) {
+                            throw new RuntimeException(e);
+                        }
+                    }, threadPool)).collect(Collectors.toList());
+            CompletableFuture.allOf(cfList.toArray(new CompletableFuture[0])).join();
+            filesSize += cfList.stream().map(CompletableFuture::join).reduce(0f, Float::sum);
         }
         return filesSize;
     }
